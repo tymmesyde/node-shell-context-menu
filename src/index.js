20d09c733f2c0b673a4c7cb0602b4269b96f140c
mfuku <daisycodes@gmail.com>
2021-04-30 16:30:35 +0900

    add additional args

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 'use strict';
-const { Registry } = require('rage-edit');
+const {Registry} = require('rage-edit');
 const SOFTWARE_CLASSES = 'HKCU\\Software\\Classes\\';
 
 exports.registerCommand = async options => {
 	if (!options) throw new Error('options are empty');
 
-	const { name, icon, command, menu } = options;
+	const {name, icon, command, menu, args} = options;
 	if (!name) throw new Error('name is not specified');
 	if (!command) throw new Error('command is not specified');
 	if (!menu) throw new Error('menuName is not specified');
+	const joinArgs = args ? (Array.isArray(args) ? args.map(arg => `"${arg}"`).join(' ') : `"${args}"`) : undefined;
 
 	try {
 		await Registry.set(`${SOFTWARE_CLASSES}*\\shell\\${name}`);
 		await Registry.set(`${SOFTWARE_CLASSES}*\\shell\\${name}`, '', menu);
 		if (icon) await Registry.set(`${SOFTWARE_CLASSES}*\\shell\\${name}`, 'Icon', (icon.endsWith('.exe') ? `${icon},0` : icon));
-		await Registry.set(`${SOFTWARE_CLASSES}*\\shell\\${name}\\command`, '', `"${command}" "%1"`);
+		await Registry.set(`${SOFTWARE_CLASSES}*\\shell\\${name}\\command`, '', `"${command}" ${joinArgs} "%1"`);
 	} catch (e) {
 		return Promise.reject(e);
 	}
@@ -25,16 +26,17 @@ exports.registerCommand = async options => {
 exports.registerDirectoryCommand = async options => {
 	if (!options) throw new Error('options are empty');
 
-	const { name, icon, command, menu } = options;
+	const {name, icon, command, menu, args} = options;
 	if (!name) throw new Error('name is not specified');
 	if (!command) throw new Error('command is not specified');
 	if (!menu) throw new Error('menu is not specified');
+	const joinArgs = args ? (Array.isArray(args) ? args.map(arg => `"${arg}"`).join(' ') : `"${args}"`) : undefined;
 
 	try {
 		await Registry.set(`${SOFTWARE_CLASSES}Directory\\shell\\${name}`);
 		await Registry.set(`${SOFTWARE_CLASSES}Directory\\shell\\${name}`, '', menu);
 		if (icon) await Registry.set(`${SOFTWARE_CLASSES}Directory\\shell\\${name}`, 'Icon', (icon.endsWith('.exe') ? `${icon},0` : icon));
-		await Registry.set(`${SOFTWARE_CLASSES}Directory\\shell\\${name}\\command`, '', `"${command}" "%1"`);
+		await Registry.set(`${SOFTWARE_CLASSES}Directory\\shell\\${name}\\command`, '', `"${command}" ${joinArgs} "%1"`);
 	} catch (e) {
 		return Promise.reject(e);
 	}
@@ -46,15 +48,16 @@ exports.registerOpenWithCommand = async (extensions, options) => {
 	if (!extensions || !extensions.length) throw new Error('extensions is not specified');
 	if (!options) throw new Error('options are empty');
 
-	const { name, command } = options;
+	const {name, command, args} = options;
 	if (!name) throw new Error('name is not specified');
 	if (!command) throw new Error('command is not specified');
+	const joinArgs = args ? (Array.isArray(args) ? args.map(arg => `"${arg}"`).join(' ') : `"${args}"`) : undefined;
 
 	try {
 		await Promise.all((await findExtensionNames(extensions)).map(async n => {
 			await Registry.set(`${SOFTWARE_CLASSES}${n}`);
 			await Registry.set(`${SOFTWARE_CLASSES}${n}\\shell\\${name}`);
-			await Registry.set(`${SOFTWARE_CLASSES}${n}\\shell\\${name}\\command`, '', `"${command}" "%1"`);
+			await Registry.set(`${SOFTWARE_CLASSES}${n}\\shell\\${name}\\command`, '', `"${command}" ${joinArgs} "%1"`);
 		}));
 	} catch (e) {
 		return Promise.reject(e);
@@ -100,7 +103,7 @@ exports.removeOpenWithCommand = async (extensions, name) => {
 	return Promise.resolve();
 };
 
-async function findExtensionNames (exts) {
-	const { ses_root } = await Registry.get('HKCR');
+async function findExtensionNames(exts) {
+	const {ses_root} = await Registry.get('HKCR');
 	return Promise.all(Object.keys(ses_root).filter(e => exts.includes(e)).map(async k => (await Registry.get(`HKCR\\${k}`)).$values['']));
 }
---
+++ test/oneArg.js
@@ -0,0 +1,68 @@
+const tape = require('tape');
+const {Registry} = require('rage-edit');
+const shellContextMenu = require('../src/index');
+
+const appPath = 'C:\\Windows\\explorer.exe';
+const options = {
+	name: 'Explorer',
+	icon: appPath,
+	command: appPath,
+	menu: 'Open with Explorer',
+	args: '/e'
+};
+
+tape('registerCommand with one arg', async t => {
+	try {
+		await shellContextMenu.registerCommand();
+	} catch (e) {
+		t.equal(e.message, 'options are empty');
+	}
+
+	try {
+		await shellContextMenu.registerCommand({});
+	} catch (e) {
+		t.equal(e.message, 'name is not specified');
+	}
+
+	await shellContextMenu.registerCommand(options);
+
+	t.equal(await Registry.has(`HKCU\\Software\\Classes\\*\\shell\\${options.name}`), true);
+	t.equal(await Registry.has(`HKCU\\Software\\Classes\\*\\shell\\${options.name}\\command`), true);
+	t.equal(await Registry.get(`HKCU\\Software\\Classes\\*\\shell\\${options.name}\\command`, ''), '"C:\\Windows\\explorer.exe" "/e" "%1"');
+	t.end();
+});
+
+tape('registerDirectoryCommand with one arg', async t => {
+	try {
+		await shellContextMenu.registerDirectoryCommand();
+	} catch (e) {
+		t.equal(e.message, 'options are empty');
+	}
+
+	try {
+		await shellContextMenu.registerDirectoryCommand({});
+	} catch (e) {
+		t.equal(e.message, 'name is not specified');
+	}
+
+	await shellContextMenu.registerDirectoryCommand(options);
+
+	t.equal(await Registry.has(`HKCU\\Software\\Classes\\Directory\\shell\\${options.name}`), true);
+	t.equal(await Registry.has(`HKCU\\Software\\Classes\\Directory\\shell\\${options.name}\\command`), true);
+	t.equal(await Registry.get(`HKCU\\Software\\Classes\\Directory\\shell\\${options.name}\\command`, ''), '"C:\\Windows\\explorer.exe" "/e" "%1"');
+	t.end();
+});
+
+tape('removeCommand with one arg', async t => {
+	await shellContextMenu.removeCommand(options.name);
+
+	t.equal(await Registry.has(`HKCU\\Software\\Classes\\*\\shell\\${options.name}`), false);
+	t.end();
+});
+
+tape('removeDirectoryCommand with one arg', async t => {
+	await shellContextMenu.removeDirectoryCommand(options.name);
+
+	t.equal(await Registry.has(`HKCU\\Software\\Classes\\Directory\\shell\\${options.name}`), false);
+	t.end();
+});
